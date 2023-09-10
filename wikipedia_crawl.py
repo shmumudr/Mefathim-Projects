5e0ae45e0708c317e942a3039c670f0a18694a59
shmumudr <your.email@example.com>
2023-09-10 22:09:37 +0300

    origin

--- wikipedia_crawl.py
+++ wikipedia_crawl.py
@@ -1,3 +1,59 @@
+"""
+Wikipedia Image and Link Scraper
+=================================
+
+This Python script allows users to scrape images from a specified Wikipedia page and save them in folders.
+It also extracts random links from the page for further exploration.
+
+Usage:
+------
+1. Run the script, and it will prompt you to enter a Wikipedia page's title (e.g., "food").
+2. The script will then scrape images from that Wikipedia page and save them in a directory.
+3. It will also follow random links from the page and continue the process up to a specified depth.
+
+Dependencies:
+-------------
+- `sys`, `requests`, `BeautifulSoup`: Required libraries for web scraping.
+- `random`: Used for random sampling of links and images.
+- `urllib.parse`: Used for cleaning URLs and generating valid file names.
+- `os`: Used for creating directories.
+
+Variables:
+----------
+- `USER_INPUT`: Input from the user to specify the Wikipedia page title.
+- `PATH`: The URL of the Wikipedia page to start scraping.
+- `PATH_OF_DIRECTORY`: The directory where images will be saved.
+- `max_images_from_one_page`: Maximum number of images to download from a single Wikipedia page.
+
+Functions:
+----------
+- `crawl_wikipedia(url, directory, depth, width, visited_links)`: Recursively crawls Wikipedia pages,
+extracts and saves images, and follows random links.
+- `download_image(url, name, directory)`: Downloads an image from a URL and saves it in the specified directory.
+- `find_image_links(soup)`: Finds and returns image links from the given BeautifulSoup object.
+- `get_title(soup)`: Extracts and returns the title of a Wikipedia page from the BeautifulSoup object.
+- `image_name_from_url(url)`: Generates a valid file name for an image based on its URL.
+- `find_random_url_links(url, soup, visited_links, width)`: Finds random Wikipedia page links
+- 'from the given BeautifulSoup object.
+- `create_soup(url)`: Fetches a web page, parses its HTML, and returns a BeautifulSoup object.
+
+Main Execution:
+---------------
+The script's main execution occurs when `__name__` is equal to `"__main__"`.
+It sets initial parameters and starts the scraping process.
+
+Usage Notes:
+------------
+- Ensure that you have the required libraries installed before running the script.
+- The script may take a significant amount of time to complete, depending on the depth and width settings.
+- Be mindful of potential legal and ethical considerations when scraping websites.
+
+Note:
+-----
+- You can customize the script by modifying the `max_images_from_one_page`, `width`, and `depth`
+- variables to suit your scraping needs.
+"""
+
 import sys
 import requests
 from bs4 import BeautifulSoup
@@ -6,8 +62,8 @@ import urllib.parse
 import os
 from random import sample
 
-# USER_INPUT = input("please enter wikipedia value: ")
-PATH = 'https://en.wikipedia.org/wiki/food'
+USER_INPUT = input("please enter wikipedia value: ")
+PATH = 'https://en.wikipedia.org/wiki/' + USER_INPUT
 PATH_OF_DIRECTORY = '/home/mefathim/PycharmProjects/pythonProject'
 max_images_from_one_page = 21
 
